3d81f79e3b8fbace6c3e277f961806dce643f4c3
liuyq123 <517417512@qq.com>
2020-06-04 17:04:39 -0700

    finish

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -23,44 +23,58 @@ public class SeamCarver {
         return this.height;
     }
 
+    // x: column, y: row
     public double energy(int x, int y) {
-        int deltaXR = picture().get((x + 1) % width(), y).getRed() - picture().get((x - 1 + width()) % width(), y).getRed();
-        int deltaYR = picture().get(x, (y + 1) % width()).getRed() - picture().get(x, (y - 1 + width()) % width()).getRed();
-        int deltaXG = picture().get((x + 1) % width(), y).getGreen() - picture().get((x - 1 + width()) % width(), y).getGreen();
-        int deltaYG = picture().get(x, (y + 1) % width()).getGreen() - picture().get(x, (y - 1 + width()) % width()).getGreen();
-        int deltaXB = picture().get((x + 1) % width(), y).getBlue() - picture().get((x - 1 + width()) % width(), y).getBlue();
-        int deltaYB = picture().get(x, (y + 1) % width()).getBlue() - picture().get(x, (y - 1 + width()) % width()).getBlue();
-        int xGradient = deltaXR ^ 2 + deltaXG ^ 2 + deltaXB ^ 2;
-        int yGradient = deltaYR ^ 2 + deltaYG ^ 2 + deltaYB ^ 2;
-        return (double) (xGradient + yGradient);
+        double deltaXR = picture().get((x + 1) % width(), y).getRed() - picture().get((x - 1 + width()) % width(), y).getRed();
+        double deltaYR = picture().get(x, (y + 1) % height()).getRed() - picture().get(x, (y - 1 + height()) % height()).getRed();
+        double deltaXG = picture().get((x + 1) % width(), y).getGreen() - picture().get((x - 1 + width()) % width(), y).getGreen();
+        double deltaYG = picture().get(x, (y + 1) % height()).getGreen() - picture().get(x, (y - 1 + height()) % height()).getGreen();
+        double deltaXB = picture().get((x + 1) % width(), y).getBlue() - picture().get((x - 1 + width()) % width(), y).getBlue();
+        double deltaYB = picture().get(x, (y + 1) % height()).getBlue() - picture().get(x, (y - 1 + height()) % height()).getBlue();
+        double xGradient = deltaXR * deltaXR + deltaXG * deltaXG + deltaXB * deltaXB;
+        double yGradient = deltaYR * deltaYR + deltaYG * deltaYG + deltaYB * deltaYB;
+        return xGradient + yGradient;
     }
 
     public int[] findHorizontalSeam() {
+        transpose();
+        int[] ret = findVerticalSeam();
+        transpose();
+        return ret;
+    }
+
+    private void transpose() {
+        Picture tmp = new Picture(height(), width());
+        for (int i = 0; i < width(); i++) {
+            for (int j = 0; j < height(); j++) {
+                tmp.set(j, i, picture().get(i, j));
+            }
+        }
+
+        this.p = tmp;
+        int t = width();
+        this.width = height();
+        this.height = t;
+    }
+
+
+    public int[] findVerticalSeam() {
         int[] result = new int[height()];
         double[][] minCost = new double[width()][height()];
 
+        // compute minCost for each element
         for (int i = 0; i < width(); i++) {
             minCost[i][0] = energy(i, 0);
         }
 
-        for (int i = 0; i < width(); i++) {
-            for (int j = 1; j < height(); j++) {
+        for (int j = 1; j < height(); j++){
+            for (int i = 0; i < width(); i++) {
                 if (i == 0) {
                     minCost[i][j] = energy(i, j) + Math.min(minCost[i][j - 1], minCost[i + 1][j - 1]);
-                    if (minCost[i][j - 1] < minCost[i + 1][j - 1]) {
-                        result[j - 1] = i;
-                    } else {
-                        result[j - 1] = i + 1;
-                    }
-                } if (i == width() - 1) {
+                } else if (i == width() - 1) {
                     minCost[i][j] = energy(i, j) + Math.min(minCost[i - 1][j - 1], minCost[i][j - 1]);
-                    if (minCost[i][j - 1] < minCost[i - 1][j - 1]) {
-                        result[j - 1] = i;
-                    } else {
-                        result[j - 1] = i - 1;
-                    }
                 } else {
-                    double smallest = Math.min(minCost[i - 1][j - 1], Math.min(minCost[i][j - 1], minCost[i + 1][j - 1]));
+                    double smallest = Math.min(Math.min(minCost[i][j - 1], minCost[i - 1][j - 1]),  minCost[i + 1][j - 1]);
                     minCost[i][j] = energy(i, j) + smallest;
                     if (minCost[i - 1][j - 1] == smallest) {
                         result[j - 1] = i - 1;
@@ -82,17 +96,35 @@ public class SeamCarver {
             }
         }
         result[height() - 1] = minIndex;
+
+        int i = minIndex;
+        for (int j = height() - 1; j > 0; j--) {
+            if (i == 0) {
+                if (minCost[i][j - 1] < minCost[i + 1][j - 1]) {
+                    result[j - 1] = i;
+                } else {
+                    result[j - 1] = i + 1;
+                }
+            } else if (i == width() - 1) {
+                if (minCost[i][j - 1] < minCost[i - 1][j - 1]) {
+                    result[j - 1] = i;
+                } else {
+                    result[j - 1] = i - 1;
+                }
+            } else {
+                double smallest = Math.min(Math.min(minCost[i][j - 1], minCost[i - 1][j - 1]),  minCost[i + 1][j - 1]);
+                if (minCost[i - 1][j - 1] == smallest) {
+                    result[j - 1] = i - 1;
+                } else if (minCost[i][j - 1] == smallest) {
+                    result[j - 1] = i;
+                } else {
+                    result[j - 1] = i + 1;
+                }
+            }
+            i = result[j - 1];
+        }
+
         return result;
-    }
-
-
-    public int[] findVerticalSeam() {
-        this.width = p.height();
-        this.height = p.width();
-        int[] ret = findHorizontalSeam();
-        this.height = p.height();
-        this.width = p.width();
-        return ret;
     }
 
     public void removeHorizontalSeam(int[] seam) {
