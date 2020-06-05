51c6ebc1361bc5aed980e8cf3f5045148d741f29
liuyq123 <517417512@qq.com>
2020-06-04 18:27:51 -0700

    correct?

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,71 +1,122 @@
 import edu.princeton.cs.algs4.Picture;
+import java.awt.Color;
 
 public class SeamCarver {
-    private Picture p;
+    private Picture picture;
     private int width;
     private int height;
 
+
     public SeamCarver(Picture picture) {
-        this.p = new Picture(picture);
-        this.width = p.width();
-        this.height = p.height();
+        this.picture = new Picture(picture);
+        height = picture.height();
+        width = picture.width();
+
     }
 
-    public Picture picture() {
-        return new Picture(this.p);
+    // current picture
+    public Picture picture()  {
+        return new Picture(this.picture);
     }
 
+    // width of current picture
     public int width() {
-        return this.width;
+        return width;
     }
 
+    // height of current picture
     public int height() {
-        return this.height;
+        return height;
     }
 
-    // x: column, y: row
+    private double calculateEnergiesX(int x, int y) {
+        int leftX = changeX(x, -1);
+        int rightX = changeX(x, 1);
+
+        Color left = picture.get(leftX, y);
+        Color right = picture.get(rightX, y);
+
+        double rx = Math.abs(left.getRed() - right.getRed());
+        double bx = Math.abs(left.getBlue() - right.getBlue());
+        double gx = Math.abs(left.getGreen() - right.getGreen());
+
+        return (rx * rx) + (bx * bx) + (gx * gx);
+
+    }
+
+    private double calculateEnergiesY(int x, int y) {
+        int upperY = changeY(y, -1);
+        int lowerY = changeY(y, 1);
+
+        Color up = picture.get(x, upperY);
+        Color down = picture.get(x, lowerY);
+
+        double ry = Math.abs(up.getRed() - down.getRed());
+        double by = Math.abs(up.getBlue() - down.getBlue());
+        double gy = Math.abs(up.getGreen() - down.getGreen());
+
+        return (ry * ry) + (by * by) + (gy * gy);
+    }
+
+
+
+    private int changeX(int x, int diff) {
+        if (x + diff == width) {
+            return 0;
+        } else if (x + diff < 0) {
+            return width - 1;
+        }
+
+        return x + diff;
+    }
+
+    private int changeY(int y, int diff) {
+        if (y + diff == height) {
+            return 0;
+        } else if (y + diff < 0) {
+            return height - 1;
+        }
+
+        return y + diff;
+    }
+
+
+    // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x < 0 || x >= width()) {
+        if (x < 0 || x >= width) {
             throw new java.lang.IndexOutOfBoundsException();
         }
 
-        if (y < 0 || y >= height()) {
+        if (y < 0 || y >= height) {
             throw new java.lang.IndexOutOfBoundsException();
         }
 
-        double deltaXR = this.p.get((x + 1) % width(), y).getRed() - this.p.get((x - 1 + width()) % width(), y).getRed();
-        double deltaYR = this.p.get(x, (y + 1) % height()).getRed() - this.p.get(x, (y - 1 + height()) % height()).getRed();
-        double deltaXG = this.p.get((x + 1) % width(), y).getGreen() - this.p.get((x - 1 + width()) % width(), y).getGreen();
-        double deltaYG = this.p.get(x, (y + 1) % height()).getGreen() - this.p.get(x, (y - 1 + height()) % height()).getGreen();
-        double deltaXB = this.p.get((x + 1) % width(), y).getBlue() - this.p.get((x - 1 + width()) % width(), y).getBlue();
-        double deltaYB = this.p.get(x, (y + 1) % height()).getBlue() - this.p.get(x, (y - 1 + height()) % height()).getBlue();
-        double xGradient = deltaXR * deltaXR + deltaXG * deltaXG + deltaXB * deltaXB;
-        double yGradient = deltaYR * deltaYR + deltaYG * deltaYG + deltaYB * deltaYB;
-        return xGradient + yGradient;
+        return calculateEnergiesX(x, y) + calculateEnergiesY(x, y);
     }
 
+    // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         transpose();
-        int[] ret = findVerticalSeam();
+        int[] seam = findVerticalSeam();
         transpose();
-        return ret;
+        return seam;
     }
 
     private void transpose() {
-        Picture tmp = new Picture(height(), width());
-        for (int i = 0; i < width(); i++) {
-            for (int j = 0; j < height(); j++) {
-                tmp.set(j, i, this.p.get(i, j));
+        Picture temp = new Picture(height, width);
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                temp.set(row, col, picture.get(col, row));
             }
         }
 
-        this.p = tmp;
-        int t = width();
-        this.width = height();
-        this.height = t;
+        picture = temp;
+        int t = width;
+        width = height;
+        height = t;
     }
 
-
+    // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
         int[] seam = new int[height];
         double totalEnergy = Double.MAX_VALUE;
@@ -125,32 +176,33 @@ public class SeamCarver {
         return seam;
     }
 
+    // remove horizontal seam from picture
     public void removeHorizontalSeam(int[] seam) {
-        if (validateSeam(seam)) {
-            SeamRemover rm = new SeamRemover();
-            this.p = rm.removeHorizontalSeam(p, seam);
-            this.height--;
+        if (checkSeam(seam)) {
+            this.picture = new Picture(SeamRemover.removeHorizontalSeam(this.picture, seam));
+            height--;
         } else {
             throw new IllegalArgumentException();
         }
     }
 
+    // remove vertical seam from picture
     public void removeVerticalSeam(int[] seam) {
-        if (validateSeam(seam)) {
-            SeamRemover rm = new SeamRemover();
-            this.p = rm.removeVerticalSeam(p, seam);
-            this.width--;
+        if (checkSeam(seam)) {
+            this.picture = new Picture(SeamRemover.removeVerticalSeam(this.picture, seam));
+            width--;
         } else {
             throw new IllegalArgumentException();
         }
     }
 
-    private boolean validateSeam(int[] seam) {
+    private boolean checkSeam(int[] seam) {
         for (int i = 0; i < seam.length - 1; i++) {
-            if (Math.abs(seam[1] - seam[i + 1]) > 1) {
+            if (Math.abs(seam[i] - seam[i + 1]) > 1) {
                 return false;
             }
         }
+
         return true;
     }
 }
